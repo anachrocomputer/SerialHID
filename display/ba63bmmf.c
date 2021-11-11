a7b7063356f48d430f05d0cf413bd2d22e197b76
John Honniball <coredump@virginmedia.com>
2021-11-11 18:25:02 +0000

    Fix compiler warnings.
    Use <unistd.h> and remove other unused #includes.

--- display/ba63bmmf.c
+++ display/ba63bmmf.c
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <termios.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 
 typedef unsigned char byte;
