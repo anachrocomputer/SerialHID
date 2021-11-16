7b7d1523bf6456ed16787d8cd11f0da048f5030b
John Honniball <coredump@virginmedia.com>
2021-11-16 22:08:58 +0000

    Find out what display we're connected to.
    Use ESC [ 0 c sequence. Also update to new coding standard and clear screen on exit.

--- display/ba63.c
+++ display/ba63.c
@@ -10,119 +10,156 @@
 
 int Fd = 0;
 
-const int ESC = 0x1b;
+#define ESC    (0x1b)
 
 
-void display(const char *str)
+void display(const char *const str)
 {
-   int n = strlen (str);
+   const int n = strlen(str);
    
-   if (write (Fd, str, n) != n)
-      perror ("write");
+   if (write(Fd, str, n) != n)
+      perror("write");
 }
 
 
-static int openBA63Port (const char *port)
+static int openBA63Port(const char *const port)
 {
-   int fd;
    struct termios tbuf;
    long int fdflags;
 
-   fd = open (port, O_RDWR | O_NOCTTY | O_NONBLOCK);
+   const int fd = open(port, O_RDWR | O_NOCTTY | O_NONBLOCK);
    
    if (fd < 0) {
-      perror (port);
-      exit (1);
+      perror(port);
+      exit(1);
    }
    
-   if ((fdflags = fcntl (fd, F_GETFL, NULL)) < 0) {
-      perror ("fcntl GETFL");
-      exit (1);
+   if ((fdflags = fcntl(fd, F_GETFL, NULL)) < 0) {
+      perror("fcntl GETFL");
+      exit(1);
    }
    
    fdflags &= ~O_NONBLOCK;
    
-   if (fcntl (fd, F_SETFL, fdflags) < 0) {
-      perror ("fcntl SETFL");
-      exit (1);
+   if (fcntl(fd, F_SETFL, fdflags) < 0) {
+      perror("fcntl SETFL");
+      exit(1);
    }
 
-   if (tcgetattr (fd, &tbuf) < 0) {
-      perror ("tcgetattr");
-      exit (1);
+   if (tcgetattr(fd, &tbuf) < 0) {
+      perror("tcgetattr");
+      exit(1);
    }
    
-   cfsetospeed (&tbuf, B9600);
-   cfsetispeed (&tbuf, B9600);
-   cfmakeraw (&tbuf);
+   cfsetospeed(&tbuf, B9600);
+   cfsetispeed(&tbuf, B9600);
+   cfmakeraw(&tbuf);
    
    tbuf.c_cflag |= PARENB | PARODD | CLOCAL;
    
-   if (tcsetattr (fd, TCSAFLUSH, &tbuf) < 0) {
-      perror ("tcsetattr");
-      exit (1);
+   if (tcsetattr(fd, TCSAFLUSH, &tbuf) < 0) {
+      perror("tcsetattr");
+      exit(1);
    }
    
    return (fd);
 }
 
 
-int main (int argc, char argv[])
+int readBA63(void)
 {
-   Fd = openBA63Port ("/dev/ttyUSB0");
+   char buf[2];
+   
+   if (read(Fd, buf, 1) < 0) {
+      perror("read");
+      exit(1);
+   }
+   
+   return (buf[0]);
+}
 
+
+int main(const int argc, const char *const argv[])
+{
+   int ch;
+   int i;
+   char reply[32];
+   
+   Fd = openBA63Port("/dev/ttyUSB0");
+
+   display("\x1b[0c");         // Enquire what device we're connected to
+   
+   for (i = 0; ((ch = readBA63()) != 'c') && (i < 30); i++) {
+      if (ch == ESC) {
+         reply[i++] = 'E';
+         reply[i++] = 'S';
+         reply[i]   = 'C';
+      }
+      else
+         reply[i] = ch;
+   }
+   
+   reply[i++] = ch;
+   reply[i] = '\0';
+   
+   printf("Reply is: %s\n", reply);
+   
    display("\x1bR\x03");       // Select UK character set
    display("\x1b[H\x1b[2J");   // Home cursor and clear screen
    display("UNEXPECTED ITEM IN\r\n");
    display("BAGGING AREA");
    
-   sleep (5);
+   sleep(5);
    
    display("\x1b[H\x1b[2J");
    display("NOTES ARE DISPENSED\r\n");
    display("BELOW THE SCANNER");
 
-   sleep (5);
+   sleep(5);
    
    display("\x1b[H\x1b[2J");
    display("404 CREDIT CARD\r\n");
    display("   NOT FOUND");
 
-   sleep (5);
+   sleep(5);
 
    display("\x1b[H\x1b[2J");
    display("  WARP CORE BREACH\r\n");
    display("   ** IMMINENT **");
 
-   sleep (5);
+   sleep(5);
 
    display("\x1b[H\x1b[2J");
    display("   ALL YOUR BASE\r\n");
    display("  ARE BELONG TO US");
 
-   sleep (5);
+   sleep(5);
 
    display("\x1b[H\x1b[2J");
    display("Soon may\r\n");
    display(" the Wellerman come");
-   sleep (3);
+   sleep(3);
    display("\x1b[H\x1b[2J");
    display("To bring us sugar\r\n");
    display(" and tea and rum");
 
-   sleep (5);
+   sleep(5);
 
    display("\x1b[H\x1b[2J");
    display("   Four Seasons\r\n");
    display(" Total Landscaping");
 
-   sleep (5);
+   sleep(5);
 
    display("\x1b[H\x1b[2J");
    display("        EVER\r\n");
    display("        GIVEN");
 
-   close (Fd);
+   sleep(5);
+
+   display("\x1b[H\x1b[2J");
+
+   close(Fd);
    
    return (0);
 }
